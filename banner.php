070d202b4ad1b4a8175decbe8196436aa13d4e7a
Ephantus Okumu <Okumu.Ephantus@switchmedia.ke>
2019-03-28 12:17:05 +0300

    initial commit

---
+++ banner.php
@@ -0,0 +1,46 @@
+	<div class="banner">
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+			</ol>
+			<div class="carousel-inner" role="listbox">
+				<div class="carousel-item active">
+					<div class="carousel-caption">
+						<h3>
+							Stitches
+						</h3>
+					</div>
+				</div>
+				<div class="carousel-item item2">
+					<div class="carousel-caption">
+						<h3>
+						Free the inside out
+						</h3>
+					</div>
+				</div>
+				<div class="carousel-item item3">
+					<div class="carousel-caption">
+						<h3>
+							We bring you poetry in style
+						</h3>
+					</div>
+				</div>
+				<div class="carousel-item item4">
+					<div class="carousel-caption">
+						<h3>
+							Always visit us
+						</h3>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
---
+++ footer.php
@@ -0,0 +1,86 @@
+<!--footer-->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
+					<h3>About US</h3>
+					<p><?php getshortdescription("titles"); ?></p>
+					<div class="read">
+						<a href="about.php" class="btn btn-primary read-m">Read More</a>
+					</div>
+				</div>
+				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
+					<div class="tech-btm">
+						<h3>Popular Posts</h3>
+						<?php getpopularposts("page_hits"); ?>
+					</div>
+					<div class="col-md-4 top-forms text-center mt-lg-3 mt-md-1 mt-0">
+					<span class="mx-lg-4 mx-md-2  mx-1">
+						<a href="blogadmin/index.php">
+							<i class="fas fa-lock"></i> <strong>Sign In</strong></a>
+					</span>
+					<span>
+						<a href="blogadmin/membership_signup.php">
+							<i class="far fa-user"></i> <strong>Register</strong></a>
+					</span>
+				</div>
+				</div>
+				<!-- subscribe -->
+				<div class="col-lg-4 subscribe-main footer-grid-agileits-w3ls text-left">
+					<h2>Signup to our newsletter</h2>
+					<div class="subscribe-main text-left">
+							<div class="subscribe-form">
+									<form action="#" method="post" class="subscribe_form">
+										<input class="form-control" type="email" placeholder="Enter your email..." required="">
+										<button type="submit" class="btn btn-primary submit">Submit</button>
+									</form>
+									<div class="clearfix"> </div>
+						   </div>
+						<p>We respect your privacy.No spam ever!</p>
+					</div>
+					<!-- //subscribe -->
+				</div>
+			</div>
+			<!-- footer -->
+			<div class="footer-cpy text-center">
+				<div class="footer-social">
+					<div class="copyrighttop">
+						<ul>
+							<li class="mx-3">
+								<a class="facebook" href="<?php getlinks("links","facebook");?>">
+									<i class="fab fa-facebook-f"></i>
+									<span>Facebook</span>
+								</a>
+							</li>
+							<li>
+								<a class="facebook" href="<?php getlinks("links","twitter");?>">
+									<i class="fab fa-twitter"></i>
+									<span>Twitter</span>
+								</a>
+							</li>
+							<li class="mx-3">
+								<a class="facebook" href="<?php getlinks("links","googleplus");?>">
+									<i class="fab fa-google-plus-g"></i>
+									<span>Google+</span>
+								</a>
+							</li>
+							<li>
+								<a class="facebook" href="<?php getlinks("links","pinterest");?>">
+									<i class="fab fa-pinterest-p"></i>
+									<span>Pinterest</span>
+								</a>
+							</li>
+						</ul>
+
+					</div>
+				</div>
+				<div class="w3layouts-agile-copyrightbottom">
+					<p> Stitches <?php $current=date("Y"); print_r($current);?> | Designed and developed by
+						<a href="http://www.ephantusokumu.com/">Ephantus</a>
+					</p>
+
+				</div>
+			</div>
+			<!-- //footer -->
+		</div>
+	</footer>
